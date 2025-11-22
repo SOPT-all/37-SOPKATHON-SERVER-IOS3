c922ba60e272d6bcb837747b77ad5e7ab9538167
koobontak <kbt82883@gmail.com>
2025-11-23 03:31:17 +0900

    feat: 쿼리 작성

---
+++ src/main/java/org/project/domain/diary/repository/DiaryRepository.java
@@ -0,0 +1,19 @@
+package org.project.domain.diary.repository;
+
+import io.lettuce.core.dynamic.annotation.Param;
+import org.project.domain.diary.entity.Diary;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface DiaryRepository extends JpaRepository<Diary, Long> {
+
+    @Query(value = "SELECT * FROM diary LIMIT 1 OFFSET :offset", nativeQuery = true)
+    Optional<Diary> findRandomDiary(@Param("offset") int offset);
+
+    List<Diary> findAllByUserId(Long userId);
+}
