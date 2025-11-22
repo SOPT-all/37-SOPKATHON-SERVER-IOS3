c12c0e685ec9a2dcbcad2c4ae6e171d0bcd8e7b0
koobontak <kbt82883@gmail.com>
2025-11-23 03:31:37 +0900

    feat: 유저 api 구현

--- src/main/java/org/project/domain/user/service/UserService.java
+++ src/main/java/org/project/domain/user/service/UserService.java
@@ -1,4 +1,26 @@
 package org.project.domain.user.service;
 
-public interface UserService {
+import lombok.RequiredArgsConstructor;
+import org.project.domain.user.dto.response.UserResponse;
+import org.project.domain.user.entity.User;
+import org.project.domain.user.repository.UserRepository;
+import org.project.global.api.ErrorCode;
+import org.project.global.api.GeneralException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserResponse getUserInfo(Long userId) {
+
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new GeneralException(ErrorCode.NOT_FOUND_USER));
+
+        return UserResponse.from(user);
+    }
 }
