6093e293fabab00aa8bdf593cf80b93c68ee0161
JJhwan7099 <ghks7099@naver.com>
2025-11-23 03:46:18 +0900

    chore: build.gradle 수정

--- src/main/java/org/project/domain/diary/repository/DiaryRepository.java
+++ src/main/java/org/project/domain/diary/repository/DiaryRepository.java
@@ -1,9 +1,9 @@
 package org.project.domain.diary.repository;
 
-import io.lettuce.core.dynamic.annotation.Param;
 import org.project.domain.diary.entity.Diary;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
