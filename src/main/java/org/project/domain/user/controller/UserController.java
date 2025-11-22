72b36075257f4df3b9d7e32bb4b6c280e9cabaa5
koobontak <kbt82883@gmail.com>
2025-11-23 03:31:37 +0900

    feat: 유저 api 구현

--- src/main/java/org/project/domain/user/controller/UserController.java
+++ src/main/java/org/project/domain/user/controller/UserController.java
@@ -3,25 +3,29 @@ package org.project.domain.user.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.project.domain.user.dto.response.UserResponse;
 import org.project.domain.user.service.UserService;
 import org.project.global.api.ApiResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/users")
 @Tag(name = "회원 관련 api")
 public class UserController {
 
     private final UserService userService;
 
-    @GetMapping(value = "/mypage/user")  // 유저의 이름, 사용가능한 크레딧 개수 조회
+    @GetMapping(value = "/{userId}")  // 유저의 이름, 사용가능한 크레딧 개수 조회
     @Operation(summary = "마이페이지 기본 정보 제공 API")
-    public ResponseEntity<ApiResponse<String>> hello() {
+    public ResponseEntity<ApiResponse<?>> getUserInfo(@PathVariable Long userId) {
 
-        return ResponseEntity.ok(ApiResponse.ok("hello"));
+        UserResponse response = userService.getUserInfo(userId);
+
+        return ResponseEntity.ok(ApiResponse.ok(response));
     }
 }
